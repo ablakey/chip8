9392da63c39fa5b494d5e64f8c303f8105e912d9
Andrew Blakey <ablakey@gmail.com>
2020-06-02 08:30:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -130,9 +130,6 @@ impl Debugger {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // mostly redundant
-    assert_eq!(args.len() > 0, true);
-
     if args.len() < 2 {
         println!("USAGE: {} <rom-file>", args[0]);
         return;
